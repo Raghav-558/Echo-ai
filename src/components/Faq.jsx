e35ef25348a2c2c2ee344b95f33fa90582244d82
Raghav-558 <jangraraghav9211@gmail.com>
2025-01-07 15:10:49 +0530

    nothing

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -10,7 +10,7 @@ const Faq = () => {
   }
 
   return (
-    <div className='bg-navy-blue py-[210px] max-lg:pb-[60px] max-lg:pt-[100px]'>
+    <div className='bg-navy-blue pt-[210px] max-lg:pb-[60px] max-lg:pt-[100px]'>
       <div className='max-w-[1174px] mx-auto px-4'>
         <SubHeading text={'FAQs'} myClass={'pb-[60px] max-lg:pb-10'} />
         <div className='max-w-[1022px] mx-auto flex flex-col gap-6 max-sm:gap-4'>
